47c4264e384a28ed93cfc7829572d4943ed8b7b8
Matheus Fernandes <matheussky.mfj@gmail.com>
2018-12-20 14:34:21 +0000

    adicionando e removendo itens [fix algumas particularidades] e exibindos ordanados pelas categorias na LISTA DE COMPRAS

--- helpers/geral.helper.js
+++ helpers/geral.helper.js
@@ -1,3 +1,5 @@
+const objId = require('mongoose').Types.ObjectId;
+
 // Access Control
 exports.checkIsLogged = function (req, res, next) {
     if (req.isAuthenticated()) {
@@ -6,7 +8,7 @@ exports.checkIsLogged = function (req, res, next) {
         req.flash('error_msg', 'Você não está autorizado a visualizar esta página');
         res.redirect('/login');
     }
-}
+};
 
 // Check Object is Empty
 exports.isEmpty = function (obj) {
@@ -15,4 +17,32 @@ exports.isEmpty = function (obj) {
             return false;
     }
     return true;
-}
+};
+
+// Extract Name ID e Category do ITEM
+const extractItem = function (itemSelected) {
+    let nameStart = itemSelected.indexOf("name: \'");
+    let nameEnd = itemSelected.indexOf("\',\r\n");
+    let catStart = itemSelected.indexOf("category: \'");
+    let catEnd = itemSelected.indexOf("\',\r\n  __v:");
+    let idStart = itemSelected.indexOf("\n  _id: ");
+    let idEnd = itemSelected.indexOf(",\r\n  n");
+
+    return {
+        _id: objId(itemSelected.slice(idStart + 8, idEnd)),
+        name: itemSelected.slice(nameStart + 7, nameEnd),
+        category: itemSelected.slice(catStart + 11, catEnd)
+    };
+};
+
+// Adiciona ITEM na LISTA DE COMPRAS
+exports.addItemToList = function (itemSelected, lista) {
+    let itemToAdd = extractItem(itemSelected);
+
+    if (!lista.itens.some(i => i.name === itemToAdd.name)) {
+        lista.itens.push(itemToAdd);
+    }
+    lista.save(err => {
+        if (!err) console.log("itens adicionados na lista");
+    });
+};
--- routes/listas.js
+++ routes/listas.js
@@ -2,108 +2,112 @@ const express = require('express');
 const router = express.Router();
 const helper = require('../helpers/geral.helper');
 const db = require("../models");
-const objId = require('mongoose').Types.ObjectId
 
 router.route('/')
-   // Lista as LISTAS DE COMPRAS
-   .get((req, res) => {
-      db.Lista.find({}, null, (err, listas) => {
-         if (err) return res.send(err);
-         console.log(listas[0]);
-         res.render('listas/listar', { title: 'Listas de Compras', listas });
-      });
-   })
-   // Rota que salva uma nova LISTA DE COMPRA
-   .post((req, res) => {
-      req.checkBody('name', 'Campo NOME é obrigatorio').notEmpty();
-      let errors = req.validationErrors();
-
-      if (errors) { // Caso haver ERRO, renderiza o formulário novamente com os erros
-         res.render('listas/form', { title: 'Nova Lista de Compra', errors });
-      } else {
-         let novaLista = { name: req.body.name, store: req.body.store };
-
-         db.Lista.create(novaLista, (err, lista) => {
+    // Lista as LISTAS DE COMPRAS
+    .get((req, res) => {
+        db.Lista.find({}, null, (err, listas) => {
             if (err) return res.send(err);
-            console.log(lista);
-            res.redirect('/listas');
-         });
-      }
-   });
+            res.render('listas/listar', { title: 'Listas de Compras', listas });
+        });
+    })
+    // Rota que salva uma nova LISTA DE COMPRA
+    .post((req, res) => {
+        req.checkBody('name', 'Campo NOME é obrigatorio').notEmpty();
+        let errors = req.validationErrors();
+
+        if (errors) { // Caso haver ERRO, renderiza o formulário novamente com os erros
+            res.render('listas/form', { title: 'Nova Lista de Compra', errors });
+        } else {
+            let novaLista = { name: req.body.name, store: req.body.store };
+
+            db.Lista.create(novaLista, (err, lista) => {
+                if (err) return res.send(err);
+                console.log(lista);
+                res.redirect('/listas');
+            });
+        }
+    });
 
 // Formulário para ADICIONAR nova LISTA DE COMPRAS
 router.get('/new', (req, res) => {
-   res.render('listas/form', { title: 'Nova Lista de Compra' });
+    res.render('listas/form', { title: 'Nova Lista de Compra' });
 });
 
 // Formulário para INSERIR um ITEM na LISTA DE COMPRAS
 router.get('/:id/itens/add', (req, res) => {
-   db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
-      if (err) return res.send(err);
-      db.Item.find({}, null, (err, itens) => {
-         if (err) return res.send(err);
-         res.render('listas/form-add-item', { title: 'Add Item na Lista: ', lista, itens });
-      })
-   });
+    db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
+        if (err) return res.send(err);
+        db.Item.find({}, null, (err, itens) => {
+            if (err) return res.send(err);
+            res.render('listas/form-add-item', { title: 'Add Item na Lista: ', lista, itens });
+        });
+    });
 });
 
 // Rota que adiciona os ITENS na LISTA DE COMPRAS
 router.post('/:id/itens', (req, res) => {
-   db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
-      if (err) return res.send(err);
-      req.body.selectedItens.forEach(select => {
-         var nameStart = select.indexOf("name: \'");
-         var nameEnd = select.indexOf("\',\r\n");
-         var catStart = select.indexOf("category: \'");
-         var catEnd = select.indexOf("\',\r\n  __v:");
-         var idStart = select.indexOf("\n  _id: ");
-         var idEnd = select.indexOf(",\r\n  n");
+    db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
+        if (err) return res.send(err);
+        const size = Object.getOwnPropertyNames(req.body.selectedItens);
+        if (size.length > 60) {
+            // Extract Name ID e Category do ITEM | & | Adiciona na LISTA DE COMPRAS
+            helper.addItemToList(req.body.selectedItens, lista);
+            res.redirect('/listas');
+        } else {
+            req.body.selectedItens.forEach(select => {
+                // Extract Name ID e Category do ITEM | & | Adiciona na LISTA DE COMPRAS
+                helper.addItemToList(select, lista);
+            });
+            res.redirect('/listas');
+        }
+    });
+});
 
-         var item = {
-            _id: objId(select.slice(idStart + 8, idEnd)),
-            name: select.slice(nameStart + 7, nameEnd),
-            category: select.slice(catStart + 11, catEnd)
-         };
-         if (!lista.itens.some(i => i.name === item.name)) {
-            lista.itens.push(item);
-         }
-         lista.save(err => {
-            if (!err) console.log("success");
-         });
-      });
-      res.redirect('/listas');
-   });
+// Rota que deleta ITENS na LISTA DE COMPRAS
+router.delete('/:id/itens/:name', (req, res) => {
+    db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
+        if (err) return res.send(err);
+        lista.itens = lista.itens.filter(item => {
+            if (item.name !== req.params.name) {
+                return item;
+            }
+        });
+        lista.save(err => {
+            if (!err) res.redirect('/listas');
+        });
+    });
 });
 
 // Formulário para EDITAR uma LISTA DE COMPRAS
 router.get('/:id/edit', (req, res) => {
-   db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
-      if (err) return res.send(err);
-      res.render('listas/form', { title: 'Editar Lista de Compra', lista });
-   });
+    db.Lista.findOne({ _id: req.params.id }, null, (err, lista) => {
+        if (err) return res.send(err);
+        res.render('listas/form', { title: 'Editar Lista de Compra', lista });
+    });
 });
 
 // Rotas de EDITAR e DELETAR uma LISTA DE COMPRAS
 router.route('/:id')
-   // EDITAR
-   .put((req, res, next) => {
-      const update = {
-         name: req.body.name,
-         store: req.body.store
-      };
-      db.Lista.findOneAndUpdate({ _id: req.params.id }, update, null, (err, lista) => {
-         if (err) return res.send(err);
-         req.flash('success_msg', 'Lista editada com sucesso');
-         res.redirect('/listas');
-      });
-   })
-   // DELETAR
-   .delete((req, res) => {
-      db.Lista.findOneAndDelete({ _id: req.params.id }, null, (err, lista) => {
-         if (err) return res.send(err);
-         req.flash('success_msg', 'Lista deletada com sucesso');
-         res.redirect('/listas');
-      });
-   });
+    // EDITAR
+    .put((req, res, next) => {
+        const update = {
+            name: req.body.name,
+            store: req.body.store
+        };
+        db.Lista.findOneAndUpdate({ _id: req.params.id }, update, null, (err, lista) => {
+            if (err) return res.send(err);
+            req.flash('success_msg', 'Lista editada com sucesso');
+            res.redirect('/listas');
+        });
+    })
+    // DELETAR
+    .delete((req, res) => {
+        db.Lista.findOneAndDelete({ _id: req.params.id }, null, (err, lista) => {
+            if (err) return res.send(err);
+            req.flash('success_msg', 'Lista deletada com sucesso');
+            res.redirect('/listas');
+        });
+    });
 
 module.exports = router;
